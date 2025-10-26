1152f550fc4eb3e1a31519c598c63f620a5cabfd
Fumo <45699815+fumofumoenjoyer@users.noreply.github.com>
2025-10-26 14:57:35 -0300

    Add flatpaks installation script

---
+++ flatpaks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd
+
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.brave.Browser
+flatpak install flathub com.stremio.Stremio
+flatpak install flathub org.qbittorrent.qBittorrent
+flatpak install flathub org.onlyoffice.desktopeditors
+flatpak install flathub com.obsproject.Studio
+flatpak install flathub org.vinegarhq.Sober
+flatpak install flathub com.protonvpn.www
+flatpak install flathub io.podman_desktop.PodmanDesktop
+flatpak install flathub io.mpv.Mpv
+flatpak install flathub com.github.tchx84.Flatseal
